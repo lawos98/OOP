5525c775a463358b403564773616e84cd02c54d6
lawos98 <85324038+lawos98@users.noreply.github.com>
2021-10-20 22:59:10 +0200

    +

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,3 @@
 fun main(args: Array<String>) {
     println("Witaj jutrzenko!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
 }
