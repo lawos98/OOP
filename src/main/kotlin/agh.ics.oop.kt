b965541d8e7e9815a0b5b7d9158e7761ac504ed9
lawos98 <85324038+lawos98@users.noreply.github.com>
2021-10-21 11:54:17 +0200

    +

--- src/main/kotlin/agh.ics.oop.kt
+++ src/main/kotlin/agh.ics.oop.kt
@@ -1,3 +1,12 @@
+class Vector2d constructor(x :Int ,y: Int){
+    var pos_1: Int =x
+    var pos_2: Int =y
+    fun toString(v: Vector2d)
+    {
+        val value: String ="("+v.pos_1.toString()+","+v.pos_2.toString()+")"
+    }
+}
+
 fun run(path: String){
     for (i in path){
         if(!(i.isWhitespace())) {
@@ -13,8 +22,7 @@ fun run(path: String){
 }
 
 fun main(args: Array<String>) {
-    println("Start")
-    val path= readLine().toString()
-    run(path)
-    println("Stop")
+    println("Start!")
+    var v1: Vector2d(2,5)
+
 }
