dd0defe4294afe44771a7847ef92a064cd09661d
lawos <85324038+lawos98@users.noreply.github.com>
2021-11-18 14:20:42 +0100

    +

---
+++ src/main/kotlin/Grass.kt
@@ -0,0 +1,8 @@
+class Grass(var position:Vector2d) {
+    override fun toString():String{
+        return "☷"
+    }
+    fun getPositon():Vector2d{
+        return position
+    }
+}
--- src/main/kotlin/agh_oop.kt
+++ src/main/kotlin/agh_oop.kt
@@ -15,5 +15,4 @@ fun main(args: Array<String>) {
     engine.run()
     println(map.toString())
 
-
 }
